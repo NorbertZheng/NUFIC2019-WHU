dda73a101e292d9d3a8a888b14dc56df8bcc4766
Fassial <2508440340@qq.com>
2019-10-25 16:17:43 +0800

    source_code: add tools/sim_data_generator

---
+++ source_code/tools/sim_data_generator/test_ThresholdCutter_data_generator/test_ThresholdCutter_data_generator.py
@@ -0,0 +1,48 @@
+wr_str_b51 = "\t\ttx_vld = 1'b1;\n" + \
+"\t\ttx_data = 88'h55_50_" + \
+"00_00_00_00_00_00_" + \
+"00_00_00;\n" + \
+"\t\t# 100;\n" + \
+"\t\ttx_vld = 1'b0;\n" + \
+"\t\t# 39900;\t\t// 400 - period\n"
+
+wr_str_a51 = "\t\ttx_vld = 1'b1;\n" + \
+"\t\ttx_data = 88'h55_52_" + \
+"00_00_00_00_00_00_" + \
+"00_00_00;\n" + \
+"\t\t# 100;\n" + \
+"\t\ttx_vld = 1'b0;\n" + \
+"\t\t# 39900;\t\t// 400 - period\n" + \
+"\t\ttx_vld = 1'b1;\n" + \
+"\t\ttx_data = 88'h55_53_" + \
+"00_00_00_00_00_00_" + \
+"00_00_00;\n" + \
+"\t\t# 100;\n" + \
+"\t\ttx_vld = 1'b0;\n" + \
+"\t\t# 39900;\t\t// 400 - period\n"
+
+def generate_test_ThresholdCutter_data(zero_flag = 0):
+	i = 0;
+	with open("test_ThresholdCutter_data.dat", "a+") as f:
+		while i <= 0xff:
+			wr_str = "\t\ttx_vld = 1'b1;\n" + \
+			"\t\ttx_data = 88'h55_51_"
+			if not zero_flag:
+				if i >= 16:
+					wr_str += hex(i)[2:] + "_20_" + hex(i)[2:] + "_20_" + hex(i)[2:] + "_20_"
+				else:
+					wr_str += "0" + hex(i)[2] + "_20_" + "0" + hex(i)[2] + "_20_" + "0" + hex(i)[2] + "_20_"
+			else:
+				wr_str += "00_00_00_00_00_00_"
+			i += 1
+			wr_str += "00_00_00;\n" + \
+			"\t\t# 100;\n" + \
+			"\t\ttx_vld = 1'b0;\n" + \
+			"\t\t# 39900;\t\t// 400 - period\n"
+			f.write(wr_str_b51 + wr_str + wr_str_a51)
+
+def main():
+	generate_test_ThresholdCutter_data(zero_flag = input("zero_flag: "))
+
+if __name__ == "__main__":
+	main()
