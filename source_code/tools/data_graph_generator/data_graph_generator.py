f849068ecd575933528cb4e3824ac311a8b41b79
Fassial <2508440340@qq.com>
2019-11-08 23:02:35 +0800

    source_code: modify tools/data_graph_generator

--- source_code/tools/data_graph_generator/data_graph_generator.py
+++ source_code/tools/data_graph_generator/data_graph_generator.py
@@ -13,7 +13,7 @@ AX_LOW = 8
 AZ_HIGH_PLUS1 = 14
 ROLL_LOW = 24
 YAW_HIGH_PLUS1 = 30
-DATAFILE = "5.csv"
+DATAFILE = "2.csv"
 
 def data_graph_generator(datafile):
 	alldata = []
@@ -30,9 +30,10 @@ def data_graph_generator(datafile):
 		plotDisp3D(loc)
 
 def data_graph(data):
-	all_A = get_all_A(data)
-	print(all_A)
-	all_loc = get_all_loc(all_A)
+	all_A, stop_ptr = get_all_A(data)
+	for i in all_A.tolist():
+		print(i)
+	all_loc = get_all_loc(all_A, stop_ptr)
 	return all_loc
 
 def plotDisp3D(all_loc):
@@ -50,13 +51,38 @@ def plotDisp2D(all_loc):
 	plt.savefig("THREE2TWO.png")
 	plt.show()
 
-def get_g_acceleration(angle):
+def Euler2Rotation(phi, theta, psi):
+	'''
+	@Param:
+		phi		: Yaw
+		theta	: Pitch
+		psi		: Roll
+	@Ret:
+		Rotation Matrix
+	'''
+	RotationMatrix = np.zeros((3, 3)).astype(np.float64)
+	RotationMatrix[0, 0] = np.cos(theta) * np.cos(phi)
+	RotationMatrix[1, 0] = np.cos(theta) * np.sin(phi)
+	RotationMatrix[2, 0] = -np.sin(theta)
+	RotationMatrix[0, 1] = (np.sin(psi) * np.sin(theta) * np.cos(phi)) - (np.cos(psi) * np.sin(phi))
+	RotationMatrix[1, 1] = (np.sin(psi) * np.sin(theta) * np.sin(phi)) + (np.cos(psi) * np.cos(phi))
+	RotationMatrix[2, 1] = np.sin(psi) * np.cos(theta)
+	RotationMatrix[0, 2] = (np.cos(psi) * np.sin(theta) * np.cos(phi)) + (np.sin(psi) * np.sin(phi))
+	RotationMatrix[1, 2] = (np.cos(psi) * np.sin(theta) * np.sin(phi)) - (np.sin(psi) * np.cos(phi))
+	RotationMatrix[2, 2] = np.cos(psi) * np.cos(theta)
+	return RotationMatrix
+
+def get_g_acceleration(angle, stop_ptr):
 	g_acceleration = np.zeros(np.shape(angle))
 	# print(np.shape(g_acceleration))
-	g_acceleration[:, 2] = np.cos(angle[:, 2]) * g
-	# print((np.sin(angle[:, 2]) * g))
-	g_acceleration[:, 0] = (np.sin(angle[:, 2]) * g) * np.sin(angle[:, 0])
-	g_acceleration[:, 1] = (np.sin(angle[:, 2]) * g) * np.sin(angle[:, 1])
+	for i in range(stop_ptr):
+		rotationMat = Euler2Rotation(angle[i, 2], angle[i, 1], angle[i, 0])
+		g_acceleration[i, 0] = rotationMat[0, 2] * g
+		g_acceleration[i, 1] = rotationMat[1, 2] * g
+		g_acceleration[i, 2] = rotationMat[2, 2] * g
+	# for i in range(np.shape(g_acceleration)[0]):
+	# 	test = math.sqrt(g_acceleration[i, 0] ** 2 + g_acceleration[i, 1] ** 2 + g_acceleration[i, 2] ** 2)
+	# 	print(test)
 	return g_acceleration
 
 def get_all_A(data):
@@ -74,7 +100,7 @@ def get_all_A(data):
 			stop_ptr = i
 			break
 	# print(angle)
-	g_acceleration = get_g_acceleration(angle)
+	g_acceleration = get_g_acceleration(angle, stop_ptr)
 	# print(g_acceleration)
 	# get acceleration
 	acceleration = data[:, AX_LOW:AZ_HIGH_PLUS1]
@@ -84,16 +110,21 @@ def get_all_A(data):
 		acceleration[i, 2] = (acceleration[i, 4] + ((acceleration[i, 5]) << 8)) if ((acceleration[i, 5]) < (2 ** 7)) else -((2 ** 16) - (acceleration[i, 4] + ((acceleration[i, 5]) << 8)))
 	acceleration = (acceleration[:, 0:3]).astype(np.float64)
 	acceleration = acceleration / 32768 * 16 * g
+	for i in acceleration.tolist():
+		print(i)
 	# for i in A.tolist():
 	# 	print(i)
 	acceleration -= g_acceleration
-	return acceleration
+	return acceleration, stop_ptr
 
-def get_all_loc(all_A):
+def get_all_loc(all_A, stop_ptr):
 	all_loc = np.zeros(np.shape(all_A))
 	all_v = np.zeros(np.shape(all_A))
-	for i in range(1, np.shape(all_A)[0]):
+	for i in range(1, stop_ptr):
 		all_loc[i, :], all_v[i, :] = update_loc(all_loc[i - 1, :], all_v[i - 1, :], all_A[i - 1, :])
+	if stop_ptr < np.shape(all_A)[0]:
+		for i in range(stop_ptr, np.shape(all_A)[0]):
+			all_loc[i, :], all_v[i, :] = all_loc[i - 1, :], all_v[i - 1, :]
 	# print(all_loc, all_v)
 	return all_loc
 
